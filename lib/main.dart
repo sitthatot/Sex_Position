653610815fa1ee340c33aa5f78bd9a61d0124eff
sitthatot <sitthatot@gmail.com>
2023-08-12 23:55:08 +0700

    center

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,28 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("SEX POSITION"),
-          backgroundColor: Color(0xFF1A1A1A)
-        ),
-        body: Center(
-          child: Text("สวัสดีครับทุกคน Center"),
-        ),
-      ),
+          appBar: AppBar(
+              title: Text("SEX POSITION"), backgroundColor: Color(0xFF1A1A1A)),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  height: 150,
+                  width: 200,
+                  color: Colors.green[200],
+                  alignment: Alignment.center,
+                  child: const Text('NO 31'),
+                ),
+              ),
+            ],
+          )),
       theme: ThemeData(scaffoldBackgroundColor: Color(0xFF660000)),
     );
   }
